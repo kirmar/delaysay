058306365bd5c1e19ffa18afdd1a49a60a2fe393
Kira Hammond <kirmar@stormyriver.com>
2020-09-28 22:55:23 -0700

    Revert to aws_encryption_sdk v1.4.1 (until can follow https://docs.aws.amazon.com/encryption-sdk/latest/developer-guide/migration-guide.html)

--- code-slack-slash-command/User.py
+++ code-slack-slash-command/User.py
@@ -7,8 +7,7 @@ import aws_encryption_sdk
 from DelaySayExceptions import UserAuthorizeError
 from datetime import timezone, timedelta
 
-encryption_client = aws_encryption_sdk.EncryptionSDKClient()
-kms_key_provider = aws_encryption_sdk.StrictAwsKmsMasterKeyProvider(key_ids=[
+kms_key_provider = aws_encryption_sdk.KMSMasterKeyProvider(key_ids=[
     os.environ['KMS_MASTER_KEY_ARN']
 ])
 
@@ -17,14 +16,14 @@ DATETIME_FORMAT = "%Y-%m-%dT%H:%M:%SZ"
 
 def encrypt_oauth_token(token):
     token_as_bytes = token.encode()
-    encrypted_token, encryptor_header = encryption_client.encrypt(
+    encrypted_token, encryptor_header = aws_encryption_sdk.encrypt(
         source=token_as_bytes,
         key_provider=kms_key_provider
     )
     return encrypted_token
 
 def decrypt_oauth_token(encrypted_token):
-    token_as_bytes, decryptor_header = encryption_client.decrypt(
+    token_as_bytes, decryptor_header = aws_encryption_sdk.decrypt(
         source=encrypted_token,
         key_provider=kms_key_provider
     )
