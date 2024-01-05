kirmar/delaysay

commit 5ae098479602cb4c097763d1cfad2525b7a7306b

Do time-consuming things only when necessary
 * Delays ~0.7 sec from importing stripe
 * Delays ~0.1 sec from creating SSM client
 * Delays ~0.6 sec from importing dateparser
 * Delays ~1.5 sec from creating DynamoDB client

code-layer-team/StripeSubscription.py
@@ -1,31 +1,36 @@
 from boto3 import client as boto3_client
-from stripe import (
-    api_key as stripe_api_key,
-    Subscription as stripe_Subscription,
-    error as stripe_error)
 from time import time
 from os import environ as os_environ
 from datetime import datetime
 
-ssm = boto3_client('ssm')
+def setup():
+    from stripe import api_key as stripe_api_key
+    ssm = boto3_client('ssm')
 
-stripe_api_key_parameter = ssm.get_parameter(
-    Name=os_environ['STRIPE_API_KEY_SSM_NAME'],
-    WithDecryption=True
-)
-stripe_api_key = stripe_api_key_parameter['Parameter']['Value']
+    stripe_api_key_parameter = ssm.get_parameter(
+        Name=os_environ['STRIPE_API_KEY_SSM_NAME'],
+        WithDecryption=True
+    )
+    stripe_api_key = stripe_api_key_parameter['Parameter']['Value']
 
-stripe_test_api_key_parameter = ssm.get_parameter(
-    Name=os_environ['STRIPE_TESTING_API_KEY_SSM_NAME'],
-    WithDecryption=True
-)
-TEST_MODE_API_KEY = stripe_test_api_key_parameter['Parameter']['Value']
+    stripe_test_api_key_parameter = ssm.get_parameter(
+        Name=os_environ['STRIPE_TESTING_API_KEY_SSM_NAME'],
+        WithDecryption=True
+    )
+    TEST_MODE_API_KEY = stripe_test_api_key_parameter['Parameter']['Value']
+    return TEST_MODE_API_KEY
 
 
 class StripeSubscription:
+
+    SETUP_DONE = False
+    TEST_MODE_API_KEY = None
     
     def __init__(self, id):
         assert id and isinstance(id, str)
+        if not StripeSubscription.SETUP_DONE:
+            StripeSubscription.TEST_MODE_API_KEY = setup()
+            StripeSubscription.SETUP_DONE = True
         self.id = id
         self.last_updated = 0
         self._refresh()
@@ -33,6 +38,9 @@ class StripeSubscription:
     def _refresh(self):
         if time() - self.last_updated < 2:
             return
+        from stripe import (
+            Subscription as stripe_Subscription,
+            error as stripe_error)
         self.last_updated = time()
         self.mode = "live"
         try:
@@ -40,7 +48,7 @@ class StripeSubscription:
         except stripe_error.InvalidRequestError:
             self.mode = "test"
             subscription = stripe_Subscription.retrieve(
-                self.id, api_key=TEST_MODE_API_KEY)
+                self.id, api_key=StripeSubscription.TEST_MODE_API_KEY)
         self.payment_status = subscription['status']
         unix_timestamp = subscription['current_period_end']
         self.next_expiration = datetime.utcfromtimestamp(unix_timestamp)

code-layer-billing-token/BillingToken.py
@@ -1,20 +1,22 @@
 from DelaySayExceptions import BillingTokenInvalidError
-from dynamodb import dynamodb_table, DATETIME_FORMAT
 from datetime import datetime
 
 class BillingToken:
     
     def __init__(self, token):
         assert isinstance(token, str) or token == None
+        from dynamodb import dynamodb_table, DATETIME_FORMAT
         if token == "" or token == None:
             raise BillingTokenInvalidError(
                 "Billing token empty: " + str(token))
+        self.table = dynamodb_table
+        self.datetime_format = DATETIME_FORMAT
         self.token = token
         self.table_entry = None
     
     def _get_table_entry(self):
         if not self.table_entry:
-            response = dynamodb_table.get_item(
+            response = self.table.get_item(
                 Key={
                     'PK': "BILLING#" + self.token,
                     'SK': "billing"
@@ -33,8 +35,8 @@ class BillingToken:
             'PK': "BILLING#" + self.token,
             'SK': "billing",
             'token': self.token,
-            'create_time': create_time.strftime(DATETIME_FORMAT),
-            'token_expiration': expiration.strftime(DATETIME_FORMAT),
+            'create_time': create_time.strftime(self.datetime_format),
+            'token_expiration': expiration.strftime(self.datetime_format),
             'team_id': team_id,
             'team_domain': team_domain,
             'user_id': user_id
@@ -42,11 +44,11 @@ class BillingToken:
         for key in list(item):
             if not item[key]:
                 del item[key]
-        dynamodb_table.put_item(Item=item)
+        self.table.put_item(Item=item)
     
     def has_expired(self):
         date = self._get_table_entry()['token_expiration']
-        token_expiration = datetime.strptime(date, DATETIME_FORMAT)
+        token_expiration = datetime.strptime(date, self.datetime_format)
         now = datetime.utcnow()
         return (token_expiration < now)
     

code-layer-team/Team.py
@@ -2,13 +2,15 @@ from time import time
 from traceback import format_exc
 from StripeSubscription import StripeSubscription
 from DelaySayExceptions import AllStripeSubscriptionsInvalid
-from dynamodb import dynamodb_table, DATETIME_FORMAT
 from datetime import datetime, timedelta
 
 class Team:
     
     def __init__(self, id):
         assert id and isinstance(id, str)
+        from dynamodb import dynamodb_table, DATETIME_FORMAT
+        self.table = dynamodb_table
+        self.datetime_format = DATETIME_FORMAT
         self.id = id
         self.last_updated = 0
         self._refresh()
@@ -18,7 +20,7 @@ class Team:
             payment_expiration_as_string = self.payment_expiration
         elif isinstance(self.payment_expiration, datetime):
             payment_expiration_as_string = (
-                self.payment_expiration.strftime(DATETIME_FORMAT))
+                self.payment_expiration.strftime(self.datetime_format))
         else:
             # This shouldn't ever happen, but if it does, I at least
             # want to know what happened.
@@ -46,7 +48,7 @@ class Team:
     
     def _update_payment_info_in_dynamodb(self):
         payment_expiration_as_string = self._get_payment_expiration_as_string()
-        dynamodb_table.update_item(
+        self.table.update_item(
             Key={
                 'PK': "TEAM#" + self.id,
                 'SK': "team"
@@ -88,7 +90,7 @@ class Team:
         if not force and time() - self.last_updated < 2:
             return
         self.last_updated = time()
-        response = dynamodb_table.get_item(
+        response = self.table.get_item(
             Key={
                 'PK': "TEAM#" + self.id,
                 'SK': "team"
@@ -106,7 +108,7 @@ class Team:
         self.is_in_dynamodb = True
         date = response['Item']['payment_expiration']
         try:
-            self.payment_expiration = datetime.strptime(date, DATETIME_FORMAT)
+            self.payment_expiration = datetime.strptime(date, self.datetime_format)
         except:
             # The expiration is probably "never".
             self.payment_expiration = date
@@ -152,8 +154,8 @@ class Team:
             'team_name': team_name,
             'team_id': self.id,
             'enterprise_id': enterprise_id,
-            'create_time': create_time.strftime(DATETIME_FORMAT),
-            'payment_expiration': trial_expiration.strftime(DATETIME_FORMAT),
+            'create_time': create_time.strftime(self.datetime_format),
+            'payment_expiration': trial_expiration.strftime(self.datetime_format),
             'payment_plan': "trial",
             'stripe_subscriptions': []
         }
@@ -162,7 +164,7 @@ class Team:
                 continue
             if not item[key]:
                 del item[key]
-        dynamodb_table.put_item(Item=item)
+        self.table.put_item(Item=item)
         self._refresh(force=True)
     
     def add_subscription(self, subscription_id):
@@ -172,7 +174,7 @@ class Team:
         self._refresh(alert_if_not_in_dynamodb=True)
         self.subscription_ids.append(subscription_id)
         self._update_payment_info()
-        dynamodb_table.update_item(
+        self.table.update_item(
             Key={
                 'PK': "TEAM#" + self.id,
                 'SK': "team"

code-layer-user/User.py
@@ -2,7 +2,6 @@ from requests import post as requests_post
 from json import loads as json_loads
 from os import environ as os_environ
 from DelaySayExceptions import UserAuthorizeError
-from dynamodb import dynamodb_table
 from datetime import timezone, timedelta
 
 from aws_encryption_sdk import (
@@ -16,9 +15,6 @@ kms_key_provider = StrictAwsKmsMasterKeyProvider(
         os_environ['KMS_MASTER_KEY_ARN']
     ]
 )
-
-# This is the format used to log dates in the DynamoDB table.
-DATETIME_FORMAT = "%Y-%m-%dT%H:%M:%SZ"
 
 def encrypt_oauth_token(token):
     token_as_bytes = token.encode()
@@ -40,6 +36,9 @@ class User:
     
     def __init__(self, id):
         assert id and isinstance(id, str)
+        from dynamodb import dynamodb_table, DATETIME_FORMAT
+        self.table = dynamodb_table
+        self.datetime_format = DATETIME_FORMAT
         self.id = id
         self._reset()
     
@@ -51,7 +50,7 @@ class User:
     
     def _reencrypt_token_with_key_commitment(self):
         token_encrypted_with_key_commitment = encrypt_oauth_token(self.token)
-        dynamodb_table.update_item(
+        self.table.update_item(
             Key={
                 'PK': "USER#" + self.id,
                 'SK': "user"
@@ -68,7 +67,7 @@ class User:
     
     def _update_billing_role_in_dynamodb(self, billing_role):
         # Update their admin status or approval to handle billing
-        dynamodb_table.update_item(
+        self.table.update_item(
             Key={
                 'PK': "USER#" + self.id,
                 'SK': "user"
@@ -110,7 +109,7 @@ class User:
         # if not force and time.time() - self.last_updated < 2:
         #     return
         # self.last_updated = time.time()
-        response = dynamodb_table.get_item(
+        response = self.table.get_item(
             Key={
                 'PK': "USER#" + self.id,
                 'SK': "user"
@@ -160,7 +159,7 @@ class User:
         return self.billing_role
     
     def is_in_dynamodb(self):
-        response = dynamodb_table.get_item(
+        response = self.table.get_item(
             Key={
                 'PK': "USER#" + self.id,
                 'SK': "user"
@@ -170,7 +169,7 @@ class User:
     
     def get_auth_token(self):
         if not self.token:
-            response = dynamodb_table.get_item(
+            response = self.table.get_item(
                 Key={
                     'PK': "USER#" + self.id,
                     'SK': "user"
@@ -220,12 +219,12 @@ class User:
             'team_name': team_name,
             'team_id': team_id,
             'enterprise_id': enterprise_id,
-            'create_time': create_time.strftime(DATETIME_FORMAT)
+            'create_time': create_time.strftime(self.datetime_format)
         }
         for key in list(item):
             if not item[key]:
                 del item[key]
-        dynamodb_table.put_item(Item=item)
+        self.table.put_item(Item=item)
         self._reset()
     
     def __eq__(self, other):

code-slack-slash-command-second-responder/SlashCommandParser.py
@@ -1,7 +1,6 @@
 from re import sub as re_sub
 from DelaySayExceptions import CommandParseError, TimeParseError
 from datetime import datetime, timedelta
-from dateparser import parse as dateparser_parse
 
 SECONDS_THRESHOLD = timedelta(minutes=10)
 
@@ -35,6 +34,7 @@ class SlashCommandParser:
         return re_sub(r"^0(?=[0-9]:)", "", time_string)
     
     def _parse_time(self):
+        from dateparser import parse as dateparser_parse
         user_input = self.original_time.rstrip(":").rstrip(",")
         user_input = user_input.replace("hr", "hour").replace("h ", "hour ")
         user_input = user_input.replace("a ", "am ")

commit 1b8422dcea7d4ac0433e3cbb8d104251c8556f55

Fix Stripe API key assignment, broken in 5ae09847960

code-layer-team/StripeSubscription.py
@@ -4,32 +4,33 @@ from os import environ as os_environ
 from datetime import datetime
 
 def setup():
-    from stripe import api_key as stripe_api_key
     ssm = boto3_client('ssm')
 
     stripe_api_key_parameter = ssm.get_parameter(
         Name=os_environ['STRIPE_API_KEY_SSM_NAME'],
         WithDecryption=True
     )
-    stripe_api_key = stripe_api_key_parameter['Parameter']['Value']
+    API_KEY = stripe_api_key_parameter['Parameter']['Value']
 
     stripe_test_api_key_parameter = ssm.get_parameter(
         Name=os_environ['STRIPE_TESTING_API_KEY_SSM_NAME'],
         WithDecryption=True
     )
     TEST_MODE_API_KEY = stripe_test_api_key_parameter['Parameter']['Value']
-    return TEST_MODE_API_KEY
+    return (API_KEY, TEST_MODE_API_KEY)
 
 
 class StripeSubscription:
 
     SETUP_DONE = False
+    API_KEY = None
     TEST_MODE_API_KEY = None
     
     def __init__(self, id):
         assert id and isinstance(id, str)
         if not StripeSubscription.SETUP_DONE:
-            StripeSubscription.TEST_MODE_API_KEY = setup()
+            (StripeSubscription.API_KEY,
+             StripeSubscription.TEST_MODE_API_KEY) = setup()
             StripeSubscription.SETUP_DONE = True
         self.id = id
         self.last_updated = 0
@@ -44,7 +45,8 @@ class StripeSubscription:
         self.last_updated = time()
         self.mode = "live"
         try:
-            subscription = stripe_Subscription.retrieve(self.id)
+            subscription = stripe_Subscription.retrieve(
+                self.id, api_key=StripeSubscription.API_KEY)
         except stripe_error.InvalidRequestError:
             self.mode = "test"
             subscription = stripe_Subscription.retrieve(